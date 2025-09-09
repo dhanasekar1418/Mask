82f3e350c7f23e9f34e769ee39b4a964c7815310
dhanasekar1418 <dhanasekar.v2022eee@sece.ac.in>
2025-09-09 23:17:32 +0530

    setup github pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
+// Change "your-repo-name" to your GitHub repo name
 export default defineConfig({
   plugins: [react()],
-})
+  base: "/Mask/"
+});
